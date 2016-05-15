d7db4a6960c4111afef5adf5a307a6648a529901
Eric BREHAULT <ebrehault@gmail.com>
2016-05-15 12:32:04 +0200

    allow to load resources from a Plone backend

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var fs = require('fs')
 var loaderUtils = require("loader-utils");
+var request = require('request');
 
 module.exports = function(content) {
     this.cacheable && this.cacheable();
@@ -9,17 +10,39 @@ module.exports = function(content) {
 module.exports.pitch = function(remainingRequest, precedingRequest, data) {
     this.cacheable && this.cacheable();
     var query = loaderUtils.parseQuery(this.query);
-    if(!query.themepath) {
-        return
-    }
     var path = this.resourcePath
-    var src_root = path.lastIndexOf('/src/');
-    var newpath = path.substring(0, src_root + 1) + query.themepath + path.substring(src_root + 4);
-    //console.log("TRYING " + newpath);
-    fs.readFile(newpath, 'utf8', function (err, newcontent) {
-        if(!err) {
-            console.log("OVERRIDE " + path + " WITH " + newpath);
-            data.override = newcontent;
-        }
-    });
+    var src_root = path.substring(0, path.lastIndexOf('/src/') + 1)
+    var resource_path = path.substring(path.lastIndexOf('/src/') + 4);
+
+    if(query.themepath) {
+        var newpath = src_root + query.themepath + resource_path;
+        // console.log("TRYING " + newpath);
+        fs.readFile(newpath, 'utf8', function (err, newcontent) {
+            if(!err) {
+                console.log("OVERRIDE " + path + " WITH " + newpath);
+                data.override = newcontent;
+            }
+        });
+    }
+
+    if(query.backend) {
+        request(
+            {
+                url: query.backend + '/theme_?resource=' + resource_path,
+                headers: {'accept': 'application/json'}
+            },
+            function (error, response, body) {
+                if (!error && response.statusCode == 200) {
+                    var resource_url = JSON.parse(body).href;
+                    // console.log("TRYING " + resource_url);
+                    request(resource_url, function (error, response, body) {
+                        if (!error && response.statusCode == 200) {
+                            console.log("OVERRIDE " + path + " WITH " + resource_url);
+                            data.override = body;
+                        }
+                    });
+                }
+            }
+        );
+    }
 }
